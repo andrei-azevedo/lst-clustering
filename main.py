f3786ffa524f8080e87a858c622ade74deef3860
Andrei Cordova Azevedo <andreicordovaazevedo@Andreis-MacBook-Air.local>
2023-12-03 22:45:57 -0300

    add eth_traffic csv

--- main.py
+++ main.py
@@ -1,47 +1,44 @@
 import os
 from scapy.all import *
-from scapy.layers.all import *
+from scapy.layers.inet import *
 
 from sklearn.cluster import KMeans
+from enum import Enum
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 import struct
+
+class PROTOCOL(Enum):
+    TCP = 0
+    UDP = 1
+
+class LABEL(Enum):
+    ETH_TRAFFIC = 0
+    NORMAL_TRAFFIC = 1
 
 def readPCAP(filepath):
     packets = PcapReader(filepath)
     i = 0
     packet_list = []
     for packet in packets:
-        try:
-            if packet.haslayer(TCP):
-                if len(str(packet[TCP].payload)) > 0:
-                    packet_list.insert(i, (0, len(packet), bytes(str(packet[TCP].payload)[0:18] + '\'', 'utf-8')))
-                else:
-                    packet_list.insert(i, (0, len(packet), '' * 16))
-            elif packet.haslayer(UDP):
-                if len(str(packet[UDP].payload)) > 0:
-                    [t] = struct.unpack('f', bytes(str(packet[UDP].payload)[0:18] + '\'', 'utf-8'))
-                    packet_list.insert(i, (0, len(packet), ))
-                else:
-                    packet_list.insert(i, (0, len(packet), '' * 16))
-        except Exception as e:
-            print(e)
-            pass
-        i += 1
-        if i > 10000:
-            break
+        if packet.haslayer(TCP):
+            if len(packet[TCP].payload) > 0:
+                packet_list.insert(i, (packet[TCP].src, packet[TCP].dst, len(packet), bytes(packet[TCP].payload), PROTOCOL.TCP, LABEL.ETH_TRAFFIC))
+            else:
+                packet_list.insert(i, (packet[TCP].src, packet[TCP].dst, len(packet), '', PROTOCOL.TCP, LABEL.ETH_TRAFFIC))
+        elif packet.haslayer(UDP):
+            if len(packet[UDP].payload) > 0:
+                packet_list.insert(i, (packet[UDP].src, packet[UDP].dst, len(packet), bytes(packet[UDP].payload), PROTOCOL.UDP, LABEL.ETH_TRAFFIC))
+            else:
+                packet_list.insert(i, (packet[UDP].src, packet[UDP].dst, len(packet), '', PROTOCOL.UDP, LABEL.ETH_TRAFFIC))
+
     X = np.array(packet_list)
-    kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
+    df = pd.DataFrame(X)
+    df.to_csv('eth_traffic.csv', index=False, header=False)
     #filter rows of original data
-    ax = plt.axes(projection ="3d")
-    ax.scatter3D(X[:,1], X[:,0], X[:,2], color=kmeans.labels_.astype(float))
-    ax.set_xlabel('X-axis', fontweight ='bold') 
-    ax.set_ylabel('Y-axis', fontweight ='bold') 
-    ax.set_zlabel('Z-axis', fontweight ='bold')
-    plt.show()
     
 def main():
-    readPCAP(os.path.join(os.getcwd(), 'file.pcap'))
+    readPCAP(os.path.join(os.getcwd(), 'file_with_transactions.pcap'))
 
 
 if __name__ == '__main__':
